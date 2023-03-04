1d6c84d4d3749996f777d3960f2c6b46dc307765
Herroa <roman.gordov@mail.ru>
2023-03-04 13:59:31 +0700

    proof

--- main.c
+++ main.c
@@ -5,6 +5,32 @@ typedef struct{
     int y;
 } point;
 
+int circletest(char *str)
+{   
+    char str2[7]="circle";
+    int flag = 1;
+    for(int i=0;i<7;i++){
+        if(str[i]!=str2[i]){
+            flag = 0;
+        }
+        if(!flag){return 0;}
+    }
+    return 1;
+}
+
+int tritest(char *str)
+{   
+    char str2[9]="triangle";
+    int flag = 1;
+    for(int i=0;i<9;i++){
+        if(str[i]!=str2[i]){
+            flag = 0;
+        }
+        if(!flag){return 0;}
+    }
+    return 2;
+}
+
 int main()
 {
     FILE *myfile;
@@ -16,6 +42,8 @@ int main()
     while (
         fscanf(myfile, "%s %d %d %f", str, &p.x, &p.y, &r) != EOF){
             printf("%s %d %d %lf\n", str, p.x, p.y, r);
+            printf("%d\n",circletest(str));
+            printf("%d\n",tritest(str));
         }
     return 0;
 }
