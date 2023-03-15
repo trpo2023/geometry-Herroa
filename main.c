da84595322bde6d6c73155e4863301a7d04b5fee
Гордов Роман Сергеевич <iv221s07@awp11.csc.local>
2023-03-15 09:18:32 +0700

    Add Pi + File open proof

--- main.c
+++ main.c
@@ -1,57 +1,62 @@
+#include <math.h>
 #include <stdio.h>
-
-typedef struct{
+#include <stdlib.h>
+typedef struct {
     int x;
     int y;
 } point;
 
-int circletest(char *str, float r)
-{   
-    char str2[7]="circle";
+int circletest(char* str, float r)
+{
+    char str2[7] = "circle";
     int flag = 1;
-    for(int i=0;i<7;i++){
-        if(str[i]!=str2[i]){
+    for (int i = 0; i < 7; i++) {
+        if (str[i] != str2[i]) {
             flag = 0;
         }
-        if(!flag){
+        if (!flag) {
             printf("Error at column 0: expected 'circle'\n");
             return 0;
-            }
+        }
     }
-    if(r<0){
+    if (r < 0) {
         printf("Error: radius less then zero\n");
         return 0;
     }
     return 1;
 }
 
-float circle_per(float r){
+float circle_per(float r)
+{
     float p;
-    p = 2*3.14*r;
+    p = 2 * M_PI * r;
     return p;
-
 }
 
-float circle_area(float r){
+float circle_area(float r)
+{
     float s;
-    s = 3.14*r*r;
+    s = M_PI * r * r;
     return s;
 }
 
 int main()
 {
-    FILE *myfile;
+    FILE* myfile;
     char str[10];
     float r;
     point p;
 
-    myfile = fopen("geometry.txt","r");
-    while (
-        fscanf(myfile, "%s %d %d %f", str, &p.x, &p.y, &r) != EOF){
-            printf("%s %d %d %lf\n", str, p.x, p.y, r);
-            if(circletest(str,r)){
-                printf("p = %lf\ns = %lf\n",circle_per(r),circle_area(r));
-            }
-        }   
+    myfile = fopen("geometry.txt", "r");
+    if (myfile == NULL) {
+        printf("Не найден файл в директории\n");
+        exit(0);
+    }
+    while (fscanf(myfile, "%s %d %d %f", str, &p.x, &p.y, &r) != EOF) {
+        printf("%s %d %d %lf\n", str, p.x, p.y, r);
+        if (circletest(str, r)) {
+            printf("p = %lf\ns = %lf\n", circle_per(r), circle_area(r));
+        }
+    }
     return 0;
 }
